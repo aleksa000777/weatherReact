dc764894d26810b523aa0a900eab3d55b9fa924c
Aleksandra Matiyev <aleksa000777@mail.ru>
2016-04-13 15:52:28 -0400

    get the day info on click

--- app/components/Detail.js
+++ app/components/Detail.js
@@ -1,9 +1,63 @@
 var React = require('react');
 var PropTypes = React.PropTypes;
+var utils = require('../helpers/utils');
+var getDate = utils.getDate;
+var convertTemp = utils.convertTemp;
+
+var styles = {
+  container: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    maxWidth: 1200,
+    margin: '50px auto'
+  },
+  dayContainer: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'column',
+    margin: 35
+  },
+  header: {
+    fontSize: 65,
+    color: '#333',
+    fontWeight: 100,
+    textAlign: 'center'
+  },
+  subheader: {
+    fontSize: 30,
+    color: '#333',
+    fontWeight: 100
+  },
+  weather: {
+    height: 130
+  }
+}
 
 function Detail(props){
-  return(
-    <p>Details</p>
+  // console.log(this.props);
+  // console.log(props.data,'heree');
+  var date = getDate(props.data.dt);
+  var icon = props.data.weather[0].icon;
+  var min = convertTemp(props.data.temp.min);
+  var max = convertTemp(props.data.temp.max);
+  var humidity = props.data.humidity;
+  var description = props.data.weather[0].description;
+  return (
+    <div style={styles.dayContainer} onClick={props.handleClick}>
+      <img style={styles.weather} src={'./app/images/weather-icons/' + icon + '.svg'} alt="Weather" />
+      <h2 style={styles.subheader}>{date}</h2>
+      <h2 style={styles.subheader}>{props.city}</h2>
+      <p style={styles.subheader}>{description} </p>
+      <p style={styles.subheader}>min temp: {min} degrees</p>
+      <p style={styles.subheader}>max temp: {max} degrees</p>
+      <p style={styles.subheader}>humidity: {humidity} </p>
+
+
+    </div>
   )
 }
 
--- app/containers/DetailContainer.js
+++ app/containers/DetailContainer.js
@@ -3,10 +3,11 @@ var Detail = require('../components/Detail');
 
 var DetailContainer = React.createClass({
   render: function(){
-    console.log(this.props.location.state.weather);
+    // console.log(this.props.location.state.weather);
+    // console.log(this.props.routeParams.city);
     return (
       <div>
-        <Detail/>
+        <Detail data={this.props.location.state.weather} city = {this.props.routeParams.city} />
       </div>
     )
   }
